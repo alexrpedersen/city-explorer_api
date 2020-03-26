c0d0ce41341f2a5552af0e0efcf920f340487c08
Alex Pedersen <alex.r.pedersen@outlook.com>
2020-03-25 22:09:59 -0700

    adds getYelp

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
-const PORT = process.env.PORT || 3001;
+const pg = require('pg');
+const PORT = process.env.PORT || 3000;
 
 
 
