a6e085f88ab9e7b66069000a559edaf64329c343
Alex Pedersen <alex.r.pedersen@outlook.com>
2020-03-25 22:04:18 -0700

    changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 
 
